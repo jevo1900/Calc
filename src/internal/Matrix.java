a770308015390bf1840fc11ae74a333b4d408b5d
jevo1900 <dev@tamdae.com>
2020-01-31 20:12:31 +0100

    summation, abstraction added, renamed vars.

--- src/internal/Matrix.java
+++ src/internal/Matrix.java
@@ -16,7 +16,6 @@ public class Matrix {
     public Matrix(int row, int col) {
         this.matrix = new Numeral[row][col];
     }
-            
 
     public void set(int row, int col, long value) {
         matrix[row - 1][col - 1] = new Numeral(value);
@@ -27,74 +26,75 @@ public class Matrix {
     }
 
     public Numeral get(int row, int col) {
-        return matrix[row-1][col-1];
+        return matrix[row - 1][col - 1];
     }
     
+    public Matrix summation(Matrix matrix) {
+        MatrixSummation(this.matrix, matrix.getMatrix());
+        return this;
+    }
+    
+    public Matrix substraction(Matrix matrix) {
+        MatrixSubstraction(this.matrix, matrix.getMatrix());
+        return this;
+    }
+
     public Matrix multiply(Matrix matrix) {
         multiplyMatrixMatrix(this.matrix, matrix.getMatrix());
         return this;
     }
-    
+
     public Matrix multiply(long value) {
         multiplyMatrixNumber(matrix, value);
         return this;
     }
 
-    private int[][] sumarDosMatrices(int[][] a, int[][] b) {
+    private Numeral[][] MatrixSummation(Numeral[][] a, Numeral[][] b) {
+        //web supose matrix are both the same size
+        Numeral sum = new Numeral(0);
+        int i, j, rowA = a.length, colA = a[0].length;
+        Matrix m = new Matrix(rowA, colA);
 
-        // En este metodo se supone que las 2 matrices tienen el mismo tamaño
-        int matrizResultante[][];
-        int i, j, filasA, columnasA;
-        filasA = a.length;
-        columnasA = a[0].length;
-
-        matrizResultante = new int[filasA][columnasA];
-
-        for (i = 0; i < filasA; i++) {
-            for (j = 0; j < columnasA; j++) {
-                matrizResultante[i][j] = a[i][j] + b[i][j];
+        for (i = 0; i < rowA; i++) {
+            for (j = 0; j < colA; j++) {
+                m.set(rowA, colA, a[i][j].add(b[i][j]));
             }
         }
-        return matrizResultante;
+        return m.getMatrix();
     }
 
-    private int[][] restarDosMatrices(int[][] a, int[][] b) {
+    private Numeral[][] MatrixSubstraction(Numeral[][] a, Numeral[][] b) {
+        //web supose matrix are both the same size
+        Numeral sum = new Numeral(0);
+        int i, j, rowA = a.length, colA = a[0].length;
+        Matrix m = new Matrix(rowA, colA);
 
-        // En este metodo se supone que las 2 matrices tienen el mismo tamaño
-        int matrizResultante[][];
-        int i, j, filasA, columnasA;
-
-        filasA = a.length;
-        columnasA = a[0].length;
-
-        matrizResultante = new int[filasA][columnasA];
-
-        for (i = 0; i < filasA; i++) {
-            for (j = 0; j < columnasA; j++) {
-                matrizResultante[i][j] = a[i][j] - b[i][j];
+        for (i = 0; i < rowA; i++) {
+            for (j = 0; j < colA; j++) {
+                m.set(rowA, colA, a[i][j].subtract(b[i][j]));
             }
         }
-        return matrizResultante;
+        return m.getMatrix();
     }
 
     private Numeral[][] multiplyMatrixMatrix(Numeral[][] a, Numeral[][] b) {
 
         Numeral sum = new Numeral(0);
-        Matrix m=new Matrix(a.length, b[0].length);
-        
+        Matrix m = new Matrix(a.length, b[0].length);
+
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < b[0].length; j++) {
                 sum.set(0);
                 for (int k = 0; k < a[0].length; k++) {
                     sum.set(a[i][k].multiply(b[k][j]));
                 }
-                m.set(i+1, j+1, sum);
+                m.set(i + 1, j + 1, sum);
             }
         }
         return m.getMatrix();
 
     }
-    
+
     private Numeral[][] multiplyMatrixNumber(Numeral[][] a, long b) {
         Numeral[][] matrizResultado = new Numeral[a.length][a[0].length];
         for (int i = 0; i != a.length; i++) {
@@ -120,9 +120,9 @@ public class Matrix {
         b.append(bars).append("┐\n");
         for (Numeral[] numerals : matrix) {
             b.append("│");
-            b.append(String.format("%-"+countSize+"s",numerals[0]));
+            b.append(String.format("%-" + countSize + "s", numerals[0]));
             for (int i = 1; i < numerals.length; i++) {
-                b.append(String.format("│%-"+countSize+"s",numerals[i]));
+                b.append(String.format("│%-" + countSize + "s", numerals[i]));
             }
             b.append("│\n");
         }
@@ -133,24 +133,22 @@ public class Matrix {
         b.append(bars).append("┘");
         return b.toString();
     }
+
     private int countSize() {
-        int n=0;
+        int n = 0;
         for (Numeral[] numerals : matrix) {
             for (Numeral numeral : numerals) {
                 int length = numeral.toString().length();
-                if(n<length) n=length;
+                if (n < length) {
+                    n = length;
+                }
             }
         }
         return n;
     }
-    
-    
 
     private Numeral[][] getMatrix() {
         return matrix;
     }
-    
-    
-
 
 }
--- src/run/Main.java
+++ src/run/Main.java
@@ -24,25 +24,22 @@ public class Main {
         n.add(1);
         System.out.println(n);
 
-
-        Matrix m = new Matrix(3, 2);
+        Matrix m = new Matrix(2, 2);
         m.set(1, 1, 1);
         m.set(1, 2, 3);
         m.set(2, 1, 4);
         m.set(2, 2, 8);
-        m.set(3, 1, 8);
-        m.set(3, 2, 8);
-
         Matrix m2 = new Matrix(2, 2);
         m2.set(1, 1, 3);
         m2.set(1, 2, 6);
         m2.set(2, 1, 10);
         m2.set(2, 2, 4);
 
-        System.out.println(m);
-        System.out.println(m2);
-//        System.out.println(m.multiply(3));
-
-        System.out.println(m.multiply(m2));
+        System.out.println("Matrix 1: \n"+m);
+        System.out.println("Matrix 2: \n"+m2);
+        System.out.println("Matrix Sumattion: \n"+m.summation(m2));
+        System.out.println("Matrix Substraction: \n"+m.substraction(m2));
+        System.out.println("Matrix - Matrix Multiplication: \n"+m.multiply(m2));
+        System.out.println("Matrix - Number Multiplication: \n"+m.multiply(3));
     }
 }
